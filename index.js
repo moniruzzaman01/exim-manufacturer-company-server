2ef721894b5538993caa481d1b61591a0490b0d9
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-05-23 04:39:48 +0600

    payment method implimentation complete

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const stripe = require("stripe")(process.env.STRIPE_CLIENT_SECRET);
 
 app.use(cors());
 app.use(express.json());
@@ -93,6 +94,37 @@ async function run() {
       const email = req.query.email;
       const result = await purchaseCollection.find({ email }).toArray();
       res.send(result);
+    });
+    app.get("/purchaseById/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await purchaseCollection.findOne(query);
+      res.send(result);
+    });
+    app.put("/purchaseById/:id", async (req, res) => {
+      const id = req.params.id;
+      const item = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: item,
+      };
+      const result = await purchaseCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+    app.post("/create-payment-intent", async (req, res) => {
+      const item = req.body;
+      const price = item.price;
+      const amount = price * 100;
+
+      if (amount > 999999) {
+        return res.send({ message: "Amount limit excess" });
+      }
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "bdt",
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
     });
   } finally {
     // await client.close();
