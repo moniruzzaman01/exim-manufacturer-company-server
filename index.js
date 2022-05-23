98fe4de0310eb8c6d470594fc459be5b738b61e9
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-05-23 23:32:13 +0600

    cancel purchase by id route added

--- index.js
+++ index.js
@@ -105,6 +105,12 @@ async function run() {
       const result = await purchaseCollection.findOne(query);
       res.send(result);
     });
+    app.delete("/purchaseById/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await purchaseCollection.deleteOne(query);
+      res.send(result);
+    });
     app.put("/purchaseById/:id", async (req, res) => {
       const id = req.params.id;
       const item = req.body;
